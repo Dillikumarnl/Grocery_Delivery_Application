2d9e6b1f788465dcecada41c806b2761f72079b2
Dillikumar <dillikumarn404@gmail.com>
2023-11-14 05:08:18 +0530

    design and backend changed

---
+++ Back_End/groceries/src/main/java/com/Nm_Grocery_Delivery_Application_4204_CA1/groceries/GroceriesApplication.java
@@ -0,0 +1,13 @@
+package com.Nm_Grocery_Delivery_Application_4204_CA1.groceries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class GroceriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceriesApplication.class, args);}
+
+}
